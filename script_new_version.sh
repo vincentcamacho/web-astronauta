50120b29f343133356d72b952b1815e6f0ca7ff6
Vincent <jvincent.camacho@gmail.com>
2022-04-30 06:50:22 +0200

    NewChanges

--- script_new_version.sh
+++ script_new_version.sh
@@ -2,24 +2,24 @@
 registro=$1
 imagen=$2
 version=$3
-contenedor=neo
+contenedor=$4
 
 # --------- COMANDOS DOCKER -----------
 docker stop $contenedor
 docker rm $contenedor
-docker rmi $registro/$imagen:'latest'
+docker rmi vincenup/$imagen:'latest'
 
 cd ~/ts/web-astronauta/
-docker build -t $registro/$imagen:$version .
-docker tag $registro/$imagen:$version $registro/$imagen:'latest'
+docker build -t vincenup/$imagen:$version .
+docker tag vincenup/$imagen:$version vincenup/$imagen:'latest'
 
 #cat ~/mi_contrasena.txt | docker login -u vincenup --password-stdin
 docker login -u "vincenup" -p "85c91b79-68d8-496a-89d2-470d97fff5a6" docker.io
 
-docker push $registro/$imagen:$version
-docker push $registro/$imagen:'latest'
+docker push vincenup/$imagen:$version
+docker push vincenup/$imagen:'latest'
 
-docker rmi $registro/$imagen:$version
-docker rmi $registro/$imagen:'latest'
+docker rmi vincenup/$imagen:$version
+docker rmi vincenup/$imagen:'latest'
 
-docker run -d --name $contenedor -p 8085:80 $registro/$imagen:'latest'
+docker run -d --name $contenedor -p 8085:80 vincenup/$imagen:'latest'
